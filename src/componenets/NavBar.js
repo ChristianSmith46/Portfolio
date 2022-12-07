66bbf25662a369c177725aac6568ed052a24d8c1
Christian Smith <christian.46.smith@gmail.com>
2022-12-07 15:59:39 -0500

    Completed react-bootstrap navbar

--- src/componenets/NavBar.js
+++ src/componenets/NavBar.js
@@ -1,55 +1,20 @@
-
+import { Navbar, Nav } from "react-bootstrap";
 
 function NavBar({ pageChangeHandler, currentPage }) {
 
     return (
-        <nav className="navbar navbar-expand-lg">
-            <span className="navbar-brand mb-0 h1">Christian Smith</span>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a
-                            href="#aboutme"
-                            onClick={() => pageChangeHandler('AboutMe')}
-                            className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
-                        >
-                            AboutMe
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
-                            href="#portfolio"
-                            onClick={() => pageChangeHandler('Portfolio')}
-                            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-                        >
-                            Portfolio
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
-                            href="#contact"
-                            onClick={() => pageChangeHandler('Contact')}
-                            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                        >
-                            Contact
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
-                            href="#resume"
-                            onClick={() => pageChangeHandler('Resume')}
-                            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                        >
-                            Resume
-                        </a>
-                    </li>
-                </ul>
-
-            </div>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand>Christian Smith</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                <Nav>
+                    <Nav.Link href="#AboutMe" onClick={() => pageChangeHandler('AboutMe')} className={currentPage === 'AboutMe' ? 'active' : ''}>About</Nav.Link>
+                    <Nav.Link href="#Contact" onClick={() => pageChangeHandler('Contact')} className={currentPage === 'Contact' ? 'active' : ''}>Contact</Nav.Link>
+                    <Nav.Link href="#Portfolio" onClick={() => pageChangeHandler('Portfolio')} className={currentPage === 'Portfolio' ? 'active' : ''}>Portfolio</Nav.Link>
+                    <Nav.Link href="#Resume" onClick={() => pageChangeHandler('Resume')} className={currentPage === 'Resume' ? 'active' : ''}>Resume</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
