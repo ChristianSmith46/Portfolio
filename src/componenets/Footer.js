345773f9424eca75a18d648fb1d023f26430fbe5
Christian Smith <christian.46.smith@gmail.com>
2022-12-07 17:59:09 -0500

    Did some html styling and added footer with social links.  Need to move styling to css file soon

--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -1,7 +1,30 @@
 
+import { Row, Col } from 'react-bootstrap';
+
+import { SocialIcon } from 'react-social-icons';
+
 function Footer() {
     return (
-        <h1>Footer</h1>
+        <div style={{ position: "fixed", bottom: 0, backgroundColor: "#0D3B66", width: '100%'}}>
+            <Row>
+                <Col md="8">
+                    <h3 style={{color: '#FAF0CA'}}>Website created by Christian Smith</h3>
+                </Col>
+                <Col md="4">
+                    <ul>
+                        <li>
+                            <SocialIcon target={'_blank'} url="https://github.com/ChristianSmith46" fgColor='#FAF0CA' bgColor='#EE964B'/>
+                        </li>
+                        <li>
+                            <SocialIcon target={'_blank'} url="https://www.linkedin.com/in/christian-smith-80b4a3243/" fgColor='#FAF0CA' bgColor='#EE964B'/>
+                        </li>
+                        <li>
+                            <SocialIcon target={'_blank'} url="https://twitter.com/CSmithTech46" fgColor='#FAF0CA' bgColor='#EE964B'/>
+                        </li>
+                    </ul>
+                </Col>
+            </Row>
+        </div>
     );
 }
 
--- src/componenets/NavBar.js
+++ src/componenets/NavBar.js
@@ -3,15 +3,15 @@ import { Navbar, Nav } from "react-bootstrap";
 function NavBar({ pageChangeHandler, currentPage }) {
 
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="md" variant="dark" style={{backgroundColor: '#0D3B66', color: '#FAF0CA'}}>
             <Navbar.Brand>Christian Smith</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                 <Nav>
-                    <Nav.Link href="#AboutMe" onClick={() => pageChangeHandler('AboutMe')} className={currentPage === 'AboutMe' ? 'active' : ''}>About</Nav.Link>
-                    <Nav.Link href="#Contact" onClick={() => pageChangeHandler('Contact')} className={currentPage === 'Contact' ? 'active' : ''}>Contact</Nav.Link>
-                    <Nav.Link href="#Portfolio" onClick={() => pageChangeHandler('Portfolio')} className={currentPage === 'Portfolio' ? 'active' : ''}>Portfolio</Nav.Link>
-                    <Nav.Link href="#Resume" onClick={() => pageChangeHandler('Resume')} className={currentPage === 'Resume' ? 'active' : ''}>Resume</Nav.Link>
+                    <Nav.Link onClick={() => pageChangeHandler('AboutMe')} className={currentPage === 'AboutMe' ? 'active' : ''}>About</Nav.Link>
+                    <Nav.Link onClick={() => pageChangeHandler('Contact')} className={currentPage === 'Contact' ? 'active' : ''}>Contact</Nav.Link>
+                    <Nav.Link onClick={() => pageChangeHandler('Portfolio')} className={currentPage === 'Portfolio' ? 'active' : ''}>Portfolio</Nav.Link>
+                    <Nav.Link onClick={() => pageChangeHandler('Resume')} className={currentPage === 'Resume' ? 'active' : ''}>Resume</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
