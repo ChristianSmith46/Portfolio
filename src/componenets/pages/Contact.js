47421aa1df7b030496f0855f28b466f3f7504e4f
Christian Smith <christian.46.smith@gmail.com>
2022-12-09 15:23:24 -0500

    Finished contact page

--- src/componenets/pages/Contact.js
+++ src/componenets/pages/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 
 function Contact() {
@@ -7,6 +7,8 @@ function Contact() {
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const [error, setError] = useState(false);
+    const [requiredField, setRequiredField] = useState({});
+    const [response, setResponse] = useState(false);
 
     const validateEmail = (email) => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -15,14 +17,19 @@ function Contact() {
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
+        setRequiredField({name, email, message});
 
         if (!validateEmail(email)) {
             setError(true);
+            setResponse(false);
+
         } else {
             setError(false);
             setName("");
             setEmail("");
             setMessage("");
+            setRequiredField({name, email, message});
+            setResponse(true);
         }
 
     }
@@ -35,20 +42,20 @@ function Contact() {
                 onSubmit={handleFormSubmit}
             >
                 <div className="col-12 col-lg-9">
-                    <label>Name:</label>
+                    <label>Name: {requiredField.name === "" && <span style={{color: "red"}}>*Required Field</span>}</label>
                     <input
                         value={name}
                         className="form-input w-100"
                         onChange={(event) => setName(event.target.value)}
                     />
-                    <label>Email:</label>
+                    <label>Email: {requiredField.email === "" && <span style={{color: "red"}}>*Required Field</span>}</label>
                     <input
                         value={email}
                         className="form-input w-100"
                         onBlur={() => setError(!validateEmail(email))}
                         onChange={(event) => setEmail(event.target.value)}
                     />
-                    <label>Message:</label>
+                    <label>Message: {requiredField.message === "" && <span style={{color: "red"}}>*Required Field</span>}</label>
                     <textarea
                         rows="4"
                         value={message}
@@ -58,19 +65,20 @@ function Contact() {
                 </div>
 
                 <div className="col-12 col-lg-3">
-                    <button className="btn btn-info btn-block py-3" type="submit">
-                        Contact Me
-                    </button>
+                    <Button type="submit" disabled={!name || !email || !message}>Contact Me</Button>
                 </div>
-                {error ?
+                {error &&
                     (
                         <div className="col-12 my-3 bg-danger text-white p-3">
                             Invalid Email Address
                         </div>
                     )
-                    :
+                }
+                {response &&
                     (
-                        <></>
+                        <div className="col-12 my-3 p-3">
+                            Thank you for the message!
+                        </div>
                     )
                 }
             </form>
