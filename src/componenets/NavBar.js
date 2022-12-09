ca0f06bc8fe7783d57f4c24f31b28a9e00c6f59c
Christian Smith <christian.46.smith@gmail.com>
2022-12-09 14:42:20 -0500

    Add a working download button for resume.  Style html, body for background. Make navbar close on click

--- src/componenets/NavBar.js
+++ src/componenets/NavBar.js
@@ -1,13 +1,16 @@
 import { Navbar, Nav } from "react-bootstrap";
+import { useState } from 'react';
 
 function NavBar({ pageChangeHandler, currentPage }) {
 
+    const [expanded, setExpanded] = useState(false)
+
     return (
-        <Navbar collapseOnSelect expand="md" variant="dark" sticky="top">
+        <Navbar expanded={expanded} expand="md" variant="dark" sticky="top">
             <Navbar.Brand>Christian Smith</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-                <Nav>
+                <Nav onClick={() => setExpanded(false)}>
                     <Nav.Link onClick={() => pageChangeHandler('AboutMe')} className={currentPage === 'AboutMe' ? 'active' : ''}>About</Nav.Link>
                     <Nav.Link onClick={() => pageChangeHandler('Portfolio')} className={currentPage === 'Portfolio' ? 'active' : ''}>Portfolio</Nav.Link>
                     <Nav.Link onClick={() => pageChangeHandler('Contact')} className={currentPage === 'Contact' ? 'active' : ''}>Contact</Nav.Link>
--- src/componenets/pages/Resume.js
+++ src/componenets/pages/Resume.js
@@ -1,6 +1,13 @@
+import { Button, Container } from 'react-bootstrap';
+
 function Resume() {
     return (
-        <h1>Resume</h1>
+        <Container>
+            <h1>Resume</h1>
+            <Button href='https://github.com/ChristianSmith46/Portfolio/raw/main/src/assets/resume2022.pdf'>Download Resume</Button>
+        </Container>
+
+
     )
 }
 
