5b5d52c75e8d765b9c75da1c7f454d377c94b03a
Christian Smith <christian.46.smith@gmail.com>
2022-12-26 22:41:40 -0500

    Clean up portfolio cards and add the technologies used

--- src/componenets/pages/Portfolio.js
+++ src/componenets/pages/Portfolio.js
@@ -14,39 +14,32 @@ const projects = [
     },
     {
         name: "Flutter",
-        description: "Flutter is a blog project I worked on with a few classmates that is intended to be for other new developers who are learning and want to share what they've learned.",
+        description: "Flutter is a blog project I worked on with a few classmates that is intended to be for other new developers who are learning and want to share what they've learned.  This project was created with Handlebars for the templating and Express on node.js for the server.",
         imageLink: "https://i.gyazo.com/843db949f3db5599bdfc07d1cd32a3f4.png",
         githubLink: "https://github.com/ChristianSmith46/Flutter",
         deployedLink: "https://flutter.herokuapp.com/"
     },
     {
         name: "Sportcast",
-        description: "Sportcast is a web app that will allow you to at your favorite NFL team's upcoming games and what the projected weather will be like at the game and who they play against.",
+        description: "Sportcast is a web app that will allow you to look at your favorite NFL team's upcoming games, what the projected weather will be like at the game and who they play against.  This app fetches the espn api and weather api then displays it using javascript dom manipulation.",
         imageLink: "https://raw.githubusercontent.com/ChristianSmith46/Sportcast/main/assets/Project1Screenshot.png",
         githubLink: "https://github.com/ChristianSmith46/Sportcast",
         deployedLink: "https://christiansmith46.github.io/Sportcast/"
     },
     {
         name: "Just Another Text Editor",
-        description: "J.A.T.E is a project that allows a user to write code/notes down that will persist when refreshed and can be downloaded for offline use.",
-        imageLink: "https://i.gyazo.com/9d3590045b6490a80acfd7a2d38d3f0c.png",
+        description: "J.A.T.E is a project that allows a user to write code/notes down that will persist when refreshed and can be downloaded for offline use.  This project is a Progressive Web Application using webpack and indexedDB.",
+        imageLink: "https://i.gyazo.com/898ed1af2260a2756dd01ed5cb6444e1.png",
         githubLink: "https://github.com/ChristianSmith46/Just-Another-Text-Editor",
         deployedLink: "https://shrouded-cove-36530.herokuapp.com/"
     },
-    {
-        name: "Daily Planner",
-        description: "Daily Planner is a web app that allows you to set notes for certain times and will color code the time to let you know which items are still to come.",
-        imageLink: "https://i.gyazo.com/218c95739781b67db24edb57dea66d7f.png",
-        githubLink: "https://github.com/ChristianSmith46/Daily-Planner",
-        deployedLink: "https://christiansmith46.github.io/Daily-Planner/"
-    },
-    {
-        name: "Timed Quiz App",
-        description: "Timed Quiz App is a web app that asks a couple questions.  You get a certain amount of time to answer and if you get a question wrong you are docked time.",
-        imageLink: "https://i.gyazo.com/00788801298279c1cbce4940d8b0ae70.png",
-        githubLink: "https://github.com/ChristianSmith46/Timed-Quiz-App",
-        deployedLink: "https://christiansmith46.github.io/Timed-Quiz-App/"
-    }
+    // {
+    //     name: "Daily Planner",
+    //     description: "Daily Planner is a web app that allows you to set notes for certain times and will color code the time to let you know which items are still to come.",
+    //     imageLink: "https://i.gyazo.com/218c95739781b67db24edb57dea66d7f.png",
+    //     githubLink: "https://github.com/ChristianSmith46/Daily-Planner",
+    //     deployedLink: "https://christiansmith46.github.io/Daily-Planner/"
+    // }
 ]
 
 function Portfolio() {
