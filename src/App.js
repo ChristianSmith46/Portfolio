7207bfc09d8edd33f70f357d12ab533e6e8e7dc7
Christian Smith <christian.46.smith@gmail.com>
2023-02-13 22:56:31 -0500

    Add resume pdf viewer to portfolio

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Content from "./componenets/Content";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Worker } from '@react-pdf-viewer/core';
 
 function App() {
     return (
-        <Content />
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.js">
+            <Content />
+        </Worker>
     );
   }
   
--- src/componenets/pages/Resume.js
+++ src/componenets/pages/Resume.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 
+// Core viewer
+import { Viewer } from '@react-pdf-viewer/core';
+
+// Plugins
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+// Import styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+import resumePDF from '../../assets/ChristianSmithResume2023.pdf';
+
+
 function Resume() {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
     return (
         <Container>
             <h1>Resume</h1>
@@ -12,7 +26,7 @@ function Resume() {
             <Row className='justify-content-center'>
                 <Col>
                     <h1>Frontend Skills</h1>
-                    <ul style={{fontSize: 30}}>
+                    <ul style={{ fontSize: 30 }}>
                         <li>React</li>
                         <li>Bootstrap</li>
                         <li>Handlebars</li>
@@ -21,7 +35,7 @@ function Resume() {
                 </Col>
                 <Col>
                     <h1>Backend Skills</h1>
-                    <ul style={{fontSize: 30}}>
+                    <ul style={{ fontSize: 30 }}>
                         <li>Express.js</li>
                         <li>MonogDb</li>
                         <li>MySQL</li>
@@ -30,6 +44,12 @@ function Resume() {
                     </ul>
                 </Col>
             </Row>
+            <Viewer
+                fileUrl={resumePDF}
+                plugins={[
+                    defaultLayoutPluginInstance,
+                ]}
+            />
         </Container>
 
 
